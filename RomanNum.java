cad124059194985e0a137bb87d0c32768d3b8d5b
HarithaMenon96 <44053924+HarithaMenon96@users.noreply.github.com>
2018-10-11 15:05:07 +0530

    Add files via upload
    Unsure of the full working. Faced difficulties regarding the same.

---
+++ RomanNum.java
@@ -0,0 +1,28 @@
+package com.deloitte;
+
+import java.util.Scanner;
+
+public class Roman {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int number;
+		System.out.println("Enter the number to converted to roman numerals (upto 3000)");
+		number = scan.nextInt();
+		String roman = ". ";
+		int[] DecimalNumbers = { 1000, 500, 100, 50, 10, 5, 1 };
+		char[] RomanNumerals = { 'M', 'D', 'C', 'L', 'X', 'V', 'I' };
+		int i;
+
+		while (number > 0) {
+			for (i = 0; i < 6; i++) {
+				if (number - DecimalNumbers[i] >= 0) {
+					System.out.print(RomanNumerals[i]);
+					number -= DecimalNumbers[i];
+					break;
+				}
+
+			}
+		}
+	}
+}
